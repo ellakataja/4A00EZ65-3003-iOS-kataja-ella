d19ca2c4250a09fc69ff77939ea2ef9200d09fda
ellakataja <97966576+ellakataja@users.noreply.github.com>
2023-05-25 18:18:26 +0300

    Update: DeleteUser.swift

--- MyProject/MyProject/DeleteUser.swift
+++ MyProject/MyProject/DeleteUser.swift
@@ -20,19 +20,20 @@ func delete(id : Int) {
         /// Prints the response to indicate success or failure
         response in print(response)
         /// Saving the response's data in a variable
-        let data = response.data
+        if let data = response.data {
         
-        /// Using JSONdecoder to parse
-        let decoder = JSONDecoder()
-        do {
-            /// Attempts to parse the JSON data as a deleted user
-            let user : User = try decoder.decode(User.self,
-            from: data!)
-            /// Prints the information of the deleted user
-            print(user)
-        } catch {
-            /// In case of failure it prints error message
-            print("Failed to decode JSON \(error.localizedDescription)")
+            /// Using JSONdecoder to parse
+            let decoder = JSONDecoder()
+            do {
+                /// Attempts to parse the JSON data as a deleted user
+                let user : User = try decoder.decode(User.self,
+                from: data)
+                /// Prints the information of the deleted user
+                print(user)
+            } catch {
+                /// In case of failure it prints error message
+                print("Failed to decode JSON \(error.localizedDescription)")
+            }
         }
     }
 }
